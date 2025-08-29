3521910039d20a4ead872b9a29fe563a5964833f
WADE <MEANTOINE12@GMAIL.COM>
2025-08-30 01:03:09 +0300

    added max number of attempts for a certain game mode

--- number guessing game/index.js
+++ number guessing game/index.js
@@ -1,27 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     let attempts = 0;
+    let max = 100
+    let min = 1
+    let mode 
 
     const submitMessage = document.querySelector("#submit-info"); // feedback area
     const inputCard = document.querySelector("#input-card");
     const numberInput = document.querySelector("#input-form input");
     const numberForm = document.querySelector("#input-form");
+    const selectMode = document.querySelector("#input-card select")
+    const heading = document.querySelector("h1")
 
     const attemptsTracker = document.querySelector("#attempts")
     attemptsTracker.innerText = 0
     
-
     // reset button
     const resetButton = document.createElement("button");
     resetButton.innerText = "Play Again";
 
     // generate random number between 1 and 100
-    const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+    const randomNumber = () => Math.floor(Math.random() * (max - min + 1)) + min;
     let num = randomNumber();
 
     // validation function
     function validateNum(input) {
-        if (isNaN(input) || input < 1 || input > 100) {
-            submitMessage.textContent = "❌ Invalid input! Enter a number between 1 and 100.";
+        if (isNaN(input) || input < min || input > max) {
+            submitMessage.textContent = `❌ Invalid input! Enter a number between ${min} and ${max}.`;
             submitMessage.style.color = "red";
             return false;
         }
@@ -39,14 +43,45 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             submitMessage.textContent = `🎉 You got it in ${attempts} attempt(s)!`;
             submitMessage.style.color = "green";
-            inputCard.innerHTML = ""; // clear form
+            inputCard.innerHTML = "" // clear form
             inputCard.append(resetButton); // show reset
         }
     }
 
+    function changeMode(e){
+        if(e.target.value === "Hard"){
+            min = 1
+            max = 1000
+            heading.textContent = `Guess a number between ${min} and ${max}`;
+            submitMessage.textContent = `🔄 New game started! Guess a number between ${min} and ${max}.`
+        }else if(e.target.value === "Easy"){
+            min = 1
+            max = 100
+            heading.textContent = `Guess a number between ${min} and ${max}`;
+            submitMessage.textContent = `🔄 New game started! Guess a number between ${min} and ${max}.`
+        }
+        num = randomNumber()
+        console.log("New number:", num);
+    }
+
+    function attemptsCount(){
+        if(attempts >= 12 && max === 1000){
+            submitMessage.textContent = "No more attempts💀Game Over!"
+            inputCard.innerHTML = ""
+            inputCard.append(resetButton)
+        }else if(attempts >= 6 && max === 100){
+            submitMessage.textContent = "No more attempts💀Game Over!"
+            inputCard.innerHTML = ""
+            inputCard.append(resetButton)
+        }
+    }
+
+
+
     // form submission
     numberForm.addEventListener("submit", (e) => {
         e.preventDefault();
+        selectMode.disabled = true
         const guess = parseInt(numberInput.value);
         if (!validateNum(guess)) return;
 
@@ -54,6 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
         attemptsTracker.innerText = attempts
         gameLogic(guess);
         numberInput.value = ""; // clear after each guess
+        attemptsCount()
     });
 
     // reset game without reload
@@ -61,10 +97,15 @@ document.addEventListener("DOMContentLoaded", () => {
         attempts = 0;
         attemptsTracker.innerText = attempts
         num = randomNumber();
-        submitMessage.textContent = "🔄 New game started! Guess a number between 1 and 100.";
+        submitMessage.textContent = `🔄 New game started! Guess a number between ${min} and ${max}.`;
         submitMessage.style.color = "black";
         inputCard.innerHTML = ""; // clear reset button
-        inputCard.append(numberForm); // restore form
+        selectMode.disabled = false
+        inputCard.append(selectMode,numberForm); // restore form
     });
+
+    selectMode.addEventListener("change",(e)=>{
+        changeMode(e)
+    })
 
 });
